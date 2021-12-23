fdc7f387f65d27b6e34b231d2213c4b10de12343
NS100 <86939732+NSFivem@users.noreply.github.com>
2021-12-23 16:46:19 -0500

    Update cl_detection.lua

--- cl_detection.lua
+++ cl_detection.lua
@@ -1,40 +1,6 @@
 
 
 
-local ped = PlayerPedId()
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1100)
-        if IsPedOnFoot and not IsPedInAnyVehicle(ped, true) then
-            RestorePlayerStamina(PlayerId(), 1.0)
-        else
-            Citizen.Wait(1600)
-        end
-    end
-end)
-
-
-Citizen.CreateThread(function()
-    while true do
-        local ped = GetPlayerPed(-1)
-
-            if HasPedGotWeapon(ped, "weapon_appistol", false) then
-                local ammo = GetMaxAmmoInClip(ped, "weapon_appistol", 1)
-
-                if ammo > 18 then
-                   TriggerServerEvent("NS100:Detection","Player has more than 18 Bullets")
-                   
-                   print("triggered, whatever whatever")
-                   
-                  -- DropPlayer("nil")
-                end
-            end
-        Citizen.Wait(2700)
-    end
-end)
-
-
 
 local ped = GetPlayerPed(-1)
 
